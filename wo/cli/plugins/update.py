3165d6d23ef12b5f5a5e1286d0abd67cb26349dc
GCT <pyrognomic@gmail.com>
2025-08-03 12:44:12 +0100

    Revert "Modified update function"
    This reverts commit 995903b6eba01687b59b81bd91a831668ed98ae8.

--- wo/cli/plugins/update.py
+++ wo/cli/plugins/update.py
@@ -1,7 +1,6 @@
 import os
 import time
-import subprocess
-import sys
+
 
 from cement.core.controller import CementBaseController, expose
 from wo.core.download import WODownload
@@ -24,101 +23,72 @@ class WOUpdateController(CementBaseController):
         arguments = [
             (['--force'],
              dict(help='Force WordOps update', action='store_true')),
-            (['--dev'],
-             dict(help='Update from the latest commit on your fork', action='store_true')),
             (['--beta'],
-             dict(help='Update WordOps to latest mainline release (same than --mainline)', action='store_true')),
+             dict(help='Update WordOps to latest mainline release '
+                  '(same than --mainline)',
+                  action='store_true')),
             (['--mainline'],
-             dict(help='Update WordOps to latest mainline release', action='store_true')),
+             dict(help='Update WordOps to latest mainline release',
+                  action='store_true')),
             (['--branch'],
-             dict(help="Update WordOps from a specific repository branch", action='store', const='develop', nargs='?')),
+                dict(help="Update WordOps from a specific repository branch ",
+                     action='store' or 'store_const',
+                     const='develop', nargs='?')),
             (['--travis'],
-             dict(help='Argument used only for WordOps development', action='store_true')),
+             dict(help='Argument used only for WordOps development',
+                  action='store_true')),
         ]
         usage = "wo update [options]"
 
     @expose(hide=True)
     def default(self):
         pargs = self.app.pargs
-
-        # Determine repo owner for fork or upstream releases
-        repo_root = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), '..', '..', '..')
-        )
-        git_url = subprocess.run(
-            ['git', 'config', '--get', 'remote.origin.url'],
-            cwd=repo_root,
-            capture_output=True,
-            text=True
-        ).stdout.strip()
-        if git_url.startswith('git@'):
-            owner = git_url.split(':', 1)[1].split('/', 1)[0]
-        else:
-            owner = git_url.rstrip('.git').split('/')[-2]
-
-        # Dev mode: pull latest commits from fork and exit
-        if pargs.dev:
-            Log.wait(self, "Pulling latest commits from fork")
-            try:
-                subprocess.run(
-                    ['git', 'pull'],
-                    cwd=repo_root,
-                    check=True,
-                    capture_output=True,
-                    text=True
-                )
-                Log.valide(self, "Pulling latest commits from fork")
-                self.app.close(0)
-            except subprocess.CalledProcessError as e:
-                Log.failed(self, "Pulling latest commits")
-                Log.error(self, e.stderr.strip())
-                sys.exit(1)
-
         filename = "woupdate" + time.strftime("%Y%m%d-%H%M%S")
-        install_args = ""
 
+        install_args = ""
         wo_branch = "master"
         if pargs.mainline or pargs.beta:
             wo_branch = "mainline"
-            install_args += "--mainline "
+            install_args = install_args + "--mainline "
         elif pargs.branch:
             wo_branch = pargs.branch
-            install_args += f"-b {wo_branch} "
+            install_args = install_args + "-b {0} ".format(wo_branch)
         if pargs.force:
-            install_args += "--force "
+            install_args = install_args + "--force "
         if pargs.travis:
-            install_args += "--travis "
+            install_args = install_args + "--travis "
             wo_branch = "updating-configuration"
 
         # check if WordOps already up-to-date
         if ((not pargs.force) and (not pargs.travis) and
             (not pargs.mainline) and (not pargs.beta) and
-            (not pargs.branch)):
-            wo_current = f"v{WOVar.wo_version}"
-            wo_latest = WODownload.latest_release(self, f"{owner}/WordOps")
+                (not pargs.branch)):
+            wo_current = ("v{0}".format(WOVar.wo_version))
+            wo_latest = WODownload.latest_release(self, "WordOps/WordOps")
             if wo_current == wo_latest:
-                Log.info(self, f"WordOps {wo_latest} is already installed")
+                Log.info(
+                    self, "WordOps {0} is already installed"
+                    .format(wo_latest))
                 self.app.close(0)
 
         # prompt user before starting upgrade
         if not pargs.force:
             Log.info(
-                self,
-                f"WordOps changelog available on https://github.com/{owner}/WordOps/releases/tag/{wo_latest}"
-            )
-            start_upgrade = input("Do you want to continue:[y/N] ")
+                self, "WordOps changelog available on "
+                "https://github.com/WordOps/WordOps/releases/tag/{0}"
+                .format(wo_latest))
+            start_upgrade = input("Do you want to continue:[y/N]")
             if start_upgrade not in ("Y", "y"):
                 Log.error(self, "Not starting WordOps update")
-                sys.exit(0)
 
-        # prepare temp directory and download update script
+        # download the install/update script
         if not os.path.isdir('/var/lib/wo/tmp'):
             os.makedirs('/var/lib/wo/tmp')
-        WODownload.download(self, [[
-            f"https://raw.githubusercontent.com/{owner}/WordOps/{wo_branch}/install",
-            f"/var/lib/wo/tmp/{filename}",
-            "update script"
-        ]])
+        WODownload.download(self, [["https://raw.githubusercontent.com/"
+                                    "WordOps/WordOps/{0}/install"
+                                    .format(wo_branch),
+                                    "/var/lib/wo/tmp/{0}".format(filename),
+                                    "update script"]])
 
         # launch install script
         if os.path.isfile('install'):
@@ -132,12 +102,13 @@ class WOUpdateController(CementBaseController):
         else:
             try:
                 Log.info(self, "updating WordOps, please wait...")
-                os.system(f"/bin/bash /var/lib/wo/tmp/{filename} {install_args}")
+                os.system("/bin/bash /var/lib/wo/tmp/{0} "
+                          "{1}".format(filename, install_args))
             except OSError as e:
                 Log.debug(self, str(e))
                 Log.error(self, "WordOps update failed !")
 
-        os.remove(f"/var/lib/wo/tmp/{filename}")
+        os.remove("/var/lib/wo/tmp/{0}".format(filename))
 
 
 def load(app):
