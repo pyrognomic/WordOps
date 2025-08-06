355283d25cc5fe5feba5447fbf09d7240d35bff7
pyrognomic <65542764+pyrognomic@users.noreply.github.com>
2025-08-06 03:34:19 +0100

    Add slug-specific auth variable

--- tests/cli/28_test_secure.py
+++ tests/cli/28_test_secure.py
@@ -23,8 +23,8 @@ class CliTestCaseSecure(TestCase):
                 with WOTestApp(argv=['secure', '--domain', 'example.com', 'user', 'pass']) as app:
                     secure_mod.load(app)
                     app.run()
-                    mock_map.assert_called_with('/etc/nginx/sites-available/example.com', ['~^/wp-login\\.php     1;', '~^/wp-admin/         1;'])
-                    mock_acl.assert_called_with('/etc/nginx/sites-available/example.com', 'example-com')
+                    mock_map.assert_called_with('/etc/nginx/sites-available/example.com', ['~^/wp-login\\.php     1;', '~^/wp-admin/         1;'], 'require_auth_example_com')
+                    mock_acl.assert_called_with('/etc/nginx/sites-available/example.com', 'example-com', 'require_auth_example_com')
 
     def test_secure_domain_whitelist(self):
         fake_site_funcs = mock.Mock()
@@ -44,5 +44,5 @@ class CliTestCaseSecure(TestCase):
                 with WOTestApp(argv=['secure', '--domain', 'example.com', '--path', '/admin', 'user', 'pass']) as app:
                     secure_mod.load(app)
                     app.run()
-                    mock_map.assert_called_with('/etc/nginx/sites-available/example.com', ['~^/admin     1;'])
-                    mock_acl.assert_called_with('/etc/nginx/sites-available/example.com', 'example-com')
+                    mock_map.assert_called_with('/etc/nginx/sites-available/example.com', ['~^/admin     1;'], 'require_auth_example_com')
+                    mock_acl.assert_called_with('/etc/nginx/sites-available/example.com', 'example-com', 'require_auth_example_com')
